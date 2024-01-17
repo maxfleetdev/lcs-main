ccdb05e9b6b459855ef4c49cd231cdc92eace78e
maxfleetdev <maxfleetdev@gmail.com>
2024-01-17 02:20:04 +0000

    Interact system update

---
+++ Client/Interaction/EventHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventHandler : MonoBehaviour
+{
+    [Header("Type of Interaction")]
+    [SerializeField] private EventType eventType;
+
+    [Header("Event on Interaction")]
+    public UnityEvent OnInteracted;
+
+    public void PlayerInteracted()
+    {
+        OnInteracted?.Invoke();
+    }
+}

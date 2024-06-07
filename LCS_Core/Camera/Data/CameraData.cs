31fab30f6119fb3dfbdce8566ca7da529f6567cb
maxfleetdev <maxfleetdev@gmail.com>
2024-06-07 02:37:05 +0100

    Camera System Redux

--- LCS_Core/Camera/Data/CameraData.cs
+++ LCS_Core/Camera/Data/CameraData.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
+using NaughtyAttributes;
+using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "LCS/Camera/Camera Data")]
 public class CameraData : ScriptableObject
 {
+    // BASE DATA //
+
+    [Header("View")]
     public ViewType CameraViewType;
+    [HorizontalLine(2, EColor.Gray)]
+
+    // TRACKING DATA //
+
+    [Header("Tracking")]
+    public bool TrackTarget;                // Tracks the target of camera
+    
+    [EnableIf("TrackTarget")]
+    public bool FocusOnPOI;                 // Will subtly track onto POI
+    
+    [HorizontalLine(2, EColor.Gray)]
+
+    // SPLINE DATA //
+
+    [ShowIf("CameraViewType", ViewType.VIEW_SPLINE)]
+    [Header("Spline Data")]
+    public List<Vector3> SplinePoints;      // Points the camera will follow
 }
--- LCS_Core/Camera/Data/ViewType.cs
+++ LCS_Core/Camera/Data/ViewType.cs
@@ -1,7 +1,8 @@
 public enum ViewType
 {
-    VIEW_FOLLOW,    // Follows the player dynamically
-    VIEW_TRACKING,    // Looks at the player constantly
-    VIEW_STATIC,    // Just stays still in a set position
-    VIEW_DOLLY      // Placeholder for now
+    VIEW_FOLLOW,   
+    VIEW_TRACKING, 
+    VIEW_STATIC,   
+    VIEW_SPLINE,
+    VIEW_DYNAMIC
 }

4bf374462f097df4e994ec26e8b8afd598a9c002
maxfleetdev <maxfleetdev@gmail.com>
2024-06-07 01:27:15 +0100

    Rechange

--- LCS_Core/Camera/CameraSystem/DynamicCameraController.cs
+++ LCS_Core/Camera/CameraSystem/DynamicCameraController.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class DynamicCameraController : MonoBehaviour
 {
-    [SerializeField] private float lookAheadOffset = 0.5f;      // offset in the z-dir
-    [SerializeField] private LayerMask obstacleLayer;           // layer which intersects with camera
-    [SerializeField] private float smoothTime = 1f;             // time of lerp when moving
-    [SerializeField] private float obstacleLength = 1.5f;
+    [SerializeField] private float smoothTime = 1f;             // time of lerp when moving camera
 
     // Transforms
     private Transform targetTransform = null;
@@ -14,22 +11,15 @@ public class DynamicCameraController : MonoBehaviour
 
     // Positioning
     private Vector3 targetPosition;
-    private Vector3 targetRotation;
-    private Vector3 targetOffset;
-    [SerializeField] private Vector3 positionOffset = new Vector3(0, 3, -4);     // default values
+    private float lookAheadOffset = 0.5f;      // offset in the z-dir
 
     // SceneCamera
     private SceneCamera activeSceneCamera = null;
     private ViewType currentViewType;
 
     // Dynamics
-    private float dynamicLookAhead = 1f;        // multiplier z-offset during freelooking
+    private Vector3 dynamicLookOffset = new Vector3(0, 0, 0);   // used to adjust the lookAheadOffset
     private bool useRuntime = false;
-    private float maxAvoidanceDistance = 2f;
-    [SerializeField] private float avoidanceStrength = 2f;       // higher = faster movement
-
-    // Cache
-    private Transform cachedTransform;
 
     #region Activation
 
@@ -38,7 +28,6 @@ public class DynamicCameraController : MonoBehaviour
     {
         if (main_target == null) return;
         targetTransform = main_target;
-        cachedTransform = transform;
     }
 
     // Called when a new SceneCamera is activated
@@ -61,12 +50,11 @@ public class DynamicCameraController : MonoBehaviour
                 StaticCamera();
                 break;
 
-            case ViewType.VIEW_LOOKAT:
+            case ViewType.VIEW_TRACKING:
                 StaticCamera();         // use static for inialising camera position/rotation
                 break;
 
             case ViewType.VIEW_FOLLOW:
-                DynamicFollowCamera();
                 break;
         }
     }
@@ -80,24 +68,19 @@ public class DynamicCameraController : MonoBehaviour
         // Runtime Check
         if (!useRuntime) return;
 
-        if (currentViewType == ViewType.VIEW_LOOKAT)
+        if (currentViewType == ViewType.VIEW_TRACKING)
         {
-            LookatCamera();
+            TrackingCamera();
             return;
         }
 
         else if (currentViewType == ViewType.VIEW_FOLLOW)
         {
-            DynamicFollowCamera();
             return;
         }
     }
 
     #endregion
-
-    //////////////////
-    // CAMERA LOGIC //
-    //////////////////
 
     #region Static Camera
 
@@ -111,79 +94,19 @@ public class DynamicCameraController : MonoBehaviour
 
     #region Lookat Camera
 
-    private void LookatCamera()
+    private void TrackingCamera()
     {
-        // Dynamic Lookahead
-        Vector3 lookAheadPosition = targetTransform.position +
-            (targetTransform.forward * lookAheadOffset * dynamicLookAhead);
-
-        // Set Positioning
-        targetPosition = lookAheadPosition + (targetTransform.rotation * targetOffset);
-
-        // Look directly at Target Position
+        // Initial Offset (preset)
+        Vector3 fixedOffset = targetTransform.forward * lookAheadOffset;
+        // Add dynamic offset to value
+        Vector3 rotatedDynamicOffset = targetTransform.rotation * dynamicLookOffset;
+        Vector3 combinedOffset = fixedOffset + rotatedDynamicOffset;
+        // Set Target Position and track its position
+        targetPosition = targetTransform.position + combinedOffset;
         transform.LookAt(targetPosition);
     }
 
-    #endregion
 
-    #region Dynamic Camera
-
-    private void DynamicFollowCamera()
-    {
-        Vector3 moveDir = targetTransform.position + AdjustedOffset();
-        moveDir += ObstacleAvoidance() * avoidanceStrength;
-        cachedTransform.position = Vector3.Lerp(transform.position, moveDir, smoothTime * Time.deltaTime);
-        LookatCamera();
-    }
-
-    // Adjusts view offset
-    private Vector3 AdjustedOffset()
-    {
-        if (!IsTargetVisible())
-        {
-            positionOffset.z = -positionOffset.z;
-        }
-        return positionOffset;
-    }
-
-    // Checks for Target Visibility
-    private bool IsTargetVisible()
-    {
-        Vector3 directionToTarget = targetTransform.position - cachedTransform.position;
-        float distanceToTarget = directionToTarget.magnitude;
-        Ray ray = new Ray(cachedTransform.position, directionToTarget.normalized);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, distanceToTarget, obstacleLayer))
-        {
-            return false;
-        }
-        return true;
-    }
-
-    // Moves camera based on surroundings
-    private Vector3 ObstacleAvoidance()
-    {
-        Vector3 avoidanceDir = Vector3.zero;
-        Vector3[] directions = {
-        cachedTransform.forward, -cachedTransform.forward,
-        cachedTransform.right, -cachedTransform.right,
-        cachedTransform.up, -cachedTransform.up
-    };
-        RaycastHit hit;
-        foreach (Vector3 dir in directions)
-        {
-            Ray ray = new Ray(cachedTransform.position, dir);
-            if (Physics.Raycast(ray, out hit, obstacleLength, obstacleLayer))
-            {
-                Debug.DrawLine(ray.origin, hit.point, Color.red);
-                float distanceToObstacle = hit.distance;
-                float avoidanceStrength = Mathf.Clamp01((maxAvoidanceDistance - distanceToObstacle) / maxAvoidanceDistance);
-                avoidanceDir += hit.normal * avoidanceStrength;
-            }
-        }
-
-        return avoidanceDir;
-    }
 
     #endregion
 
--- LCS_Core/Camera/Data/ViewType.cs
+++ LCS_Core/Camera/Data/ViewType.cs
@@ -1,7 +1,7 @@
 public enum ViewType
 {
     VIEW_FOLLOW,    // Follows the player dynamically
-    VIEW_LOOKAT,    // Looks at the player constantly
+    VIEW_TRACKING,    // Looks at the player constantly
     VIEW_STATIC,    // Just stays still in a set position
     VIEW_DOLLY      // Placeholder for now
 }
